6cc5bd6dcfe0edf7aad82b4b6a86895599b1649c
akameco <akameco.t@gmail.com>
2017-09-04 00:25:58 +0900

    Add jest timeout time

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,14 @@
 // @flow
 const captureElement = require('.')
 
-test('return Buffer', async () => {
-  const buffer = await captureElement(
-    'https://github.com/akameco',
-    '.js-calendar-graph'
-  )
-  expect(Buffer.isBuffer(buffer)).toBe(true)
-})
+test(
+  'return Buffer',
+  async () => {
+    const buffer = await captureElement(
+      'https://github.com/akameco',
+      '.js-calendar-graph'
+    )
+    expect(Buffer.isBuffer(buffer)).toBe(true)
+  },
+  10000 // add long timeout
+)
